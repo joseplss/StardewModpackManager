2a3d25471eee607a711688c644d57a722b51de52
Jose Villalobos <t1-jvillalo@vizientinc.com>
2025-07-20 23:03:27 -0500

    Main Creation - inclusion of files

---
+++ StardewModpackManager/MainFormcs.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace StardewModpackManager
+{
+    partial class MainFormcs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // MainFormcs
+            // 
+            AutoScaleDimensions = new SizeF(8F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(914, 397);
+            Font = new Font("Arial Rounded MT Bold", 8.830189F, FontStyle.Regular, GraphicsUnit.Point);
+            Name = "MainFormcs";
+            Text = "MainFormcs";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
